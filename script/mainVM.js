2ec5a72acae41cc50cf2200f622652e296fa9ede
John Warren <JWarren360@gmail.com>
2016-03-03 20:52:07 -0600

    First commit Basic layout and CSS

---
+++ script/mainVM.js
@@ -0,0 +1,59 @@
+//Main View Class
+//Changes beteen four different views.
+//-home
+//-list
+//-map
+//-search
+function MainVM() {
+	var self = this;
+	//main view shifter classes
+	self.mainClassN = ko.observable("center");
+	self.sClassN = ko.observable("disappear");
+	self.mClassN = ko.observable("appear");
+	self.lClassN = ko.observable("disappear");
+	self.sbClassN = ko.observable("");
+	//main shifter class changes
+	self.viewWindow = function (view){
+		switch(view){
+			case 1:
+				self.mainClassN("left");
+				self.sClassN("appear");
+				self.mClassN("disappear");
+				self.lClassN("disappear");
+				self.sbClassN("delayed");
+				break;
+			case 2:
+				self.mainClassN("center");
+				self.sClassN("disappear");
+				self.mClassN("appear");
+				self.lClassN("disappear");
+				self.sbClassN("");
+				break;
+			case 3:
+				self.mainClassN("right");
+				self.sClassN("disappear");
+				self.mClassN("disappear");
+				self.lClassN("appear");
+				self.sbClassN("");
+				break;
+		}
+	};
+	//button function calls
+	self.searchView = function (){
+		self.viewWindow(1);
+		console.log("search");
+	};
+	self.mapView = function (){
+		self.viewWindow(2);
+		console.log("map");
+	};
+	self.listView = function (){
+		self.viewWindow(3);
+		console.log("list");
+	};
+	self.homeView = function (){
+		self.viewWindow(2);
+		console.log("home");
+	};
+}
+ko.applyBindings(new MainVM());
---
+++ script/searchVM.js
@@ -0,0 +1,10 @@
+//console.log("search");
+//var box = document.getElementById('main');
+//var box2 = document.getElementById('search1');
+//var box3 = document.getElementById('googleMap');
+//window.getElementById('search').addEventListener.('click', toggle);
+//window.addEventListener('touchstart', toggle);
+
+//function left() {
+
+
